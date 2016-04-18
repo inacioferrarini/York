580bcd849bc1cad7101d7c150c81c9b44ce1abd1
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-04-17 22:31:29 -0300

    Updating tests. Bring back Core Data Tests.

--- Example/Tests/York/DataSyncRules/Entities/EntityHourlySyncRuleTests.swift
+++ Example/Tests/York/DataSyncRules/Entities/EntityHourlySyncRuleTests.swift
@@ -26,39 +26,39 @@ import York
 
 class EntityHourlySyncRuleTests: XCTestCase {
 
-//    func test_fetchEntityHourlySyncRule_withEmptyName_mustReturnNil() {
-//        let coreDataStack = TestUtil().appContext().coreDataStack
-//        let context = coreDataStack.managedObjectContext
-//        let hourlySincRule = EntityHourlySyncRule.fetchEntityHourlySyncRuleByName("", inManagedObjectContext: context)
-//        XCTAssertNil(hourlySincRule)
-//    }
+    func test_fetchEntityHourlySyncRule_withEmptyName_mustReturnNil() {
+        let coreDataStack = TestUtil().appContext().coreDataStack
+        let context = coreDataStack.managedObjectContext
+        let hourlySincRule = EntityHourlySyncRule.fetchEntityHourlySyncRuleByName("", inManagedObjectContext: context)
+        XCTAssertNil(hourlySincRule)
+    }
     
-//    func test_fetchEntityHourlySyncRule_withName_mustReturnEntity() {
-//        let ruleName = TestUtil().randomRuleName()
-//        let coreDataStack = TestUtil().appContext().coreDataStack
-//        let context = coreDataStack.managedObjectContext
-//        if let _ = EntityHourlySyncRule.entityHourlySyncRuleByName(ruleName, hours: nil, inManagedObjectContext:context) {
-//            let fetchedRule = EntityHourlySyncRule.fetchEntityHourlySyncRuleByName(ruleName, inManagedObjectContext: context)
-//            XCTAssertNotNil(fetchedRule)
-//        }
-//    }
+    func test_fetchEntityHourlySyncRule_withName_mustReturnEntity() {
+        let ruleName = TestUtil().randomRuleName()
+        let coreDataStack = TestUtil().appContext().coreDataStack
+        let context = coreDataStack.managedObjectContext
+        if let _ = EntityHourlySyncRule.entityHourlySyncRuleByName(ruleName, hours: nil, inManagedObjectContext:context) {
+            let fetchedRule = EntityHourlySyncRule.fetchEntityHourlySyncRuleByName(ruleName, inManagedObjectContext: context)
+            XCTAssertNotNil(fetchedRule)
+        }
+    }
     
-//    func test_entityHourlySyncRule_withEmptyName_mustReturnNil() {
-//        let coreDataStack = TestUtil().appContext().coreDataStack
-//        coreDataStack.saveContext()
-//        let context = coreDataStack.managedObjectContext
-//        let hourlySincRule = EntityHourlySyncRule.entityHourlySyncRuleByName("", hours: nil, inManagedObjectContext: context)
-//        XCTAssertNil(hourlySincRule)
-//    }
+    func test_entityHourlySyncRule_withEmptyName_mustReturnNil() {
+        let coreDataStack = TestUtil().appContext().coreDataStack
+        coreDataStack.saveContext()
+        let context = coreDataStack.managedObjectContext
+        let hourlySincRule = EntityHourlySyncRule.entityHourlySyncRuleByName("", hours: nil, inManagedObjectContext: context)
+        XCTAssertNil(hourlySincRule)
+    }
     
-//    func test_shouldRunSyncRule_withEmptyName_mustReturnNil() {
-//        let coreDataStack = TestUtil().appContext().coreDataStack
-//        let context = coreDataStack.managedObjectContext
-//        let ruleName = TestUtil().randomRuleName()
-//        if let rule = EntityHourlySyncRule.entityHourlySyncRuleByName(ruleName, hours: nil, inManagedObjectContext:context) {
-//            let shouldExecuteRule = rule.shouldRunSyncRuleWithName("", date: NSDate(), inManagedObjectContext: context)
-//            XCTAssertTrue(shouldExecuteRule)
-//        }
-//    }
+    func test_shouldRunSyncRule_withEmptyName_mustReturnNil() {
+        let coreDataStack = TestUtil().appContext().coreDataStack
+        let context = coreDataStack.managedObjectContext
+        let ruleName = TestUtil().randomRuleName()
+        if let rule = EntityHourlySyncRule.entityHourlySyncRuleByName(ruleName, hours: nil, inManagedObjectContext:context) {
+            let shouldExecuteRule = rule.shouldRunSyncRuleWithName("", date: NSDate(), inManagedObjectContext: context)
+            XCTAssertTrue(shouldExecuteRule)
+        }
+    }
     
 }
