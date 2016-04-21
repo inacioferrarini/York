bc081f444ee4e3f93fe5d5e734661715d366d807
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-04-20 23:26:44 -0300

    Class linted.

--- York/Classes/Presenter/TableViewCellPresenter.swift
+++ York/Classes/Presenter/TableViewCellPresenter.swift
@@ -25,16 +25,16 @@ import UIKit
 import CoreData
 
 public class TableViewCellPresenter<CellType: UITableViewCell, EntityType: NSManagedObject>: NSObject {
-    
-    public var configureCellBlock:((CellType, EntityType) -> Void)
-    public var cellReuseIdentifier:String
-    public var canEditRowAtIndexPathBlock:((indexPath: NSIndexPath) -> Bool)?
-    public var commitEditingStyleBlock:((editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath: NSIndexPath) -> Void)?
-       
-    public init(configureCellBlock:((CellType, EntityType) -> Void), cellReuseIdentifier:String) {
+
+    public var configureCellBlock: ((CellType, EntityType) -> Void)
+    public var cellReuseIdentifier: String
+    public var canEditRowAtIndexPathBlock: ((indexPath: NSIndexPath) -> Bool)?
+    public var commitEditingStyleBlock: ((editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath: NSIndexPath) -> Void)?
+
+    public init(configureCellBlock: ((CellType, EntityType) -> Void), cellReuseIdentifier: String) {
         self.configureCellBlock = configureCellBlock
         self.cellReuseIdentifier = cellReuseIdentifier
         super.init()
     }
-    
+
 }
