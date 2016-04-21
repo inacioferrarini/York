867c113d352467be996efb13984d5d94d760591e
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-04-20 23:42:38 -0300

    Class linted.

--- York/Classes/Extensions/UIImageExtensions.swift
+++ York/Classes/Extensions/UIImageExtensions.swift
@@ -26,11 +26,12 @@ import Foundation
 
 extension UIImage {
 
-    public class func circularScaleAndCropImage(image:UIImage, frame:CGRect) -> UIImage {
-        
-        UIGraphicsBeginImageContextWithOptions(CGSizeMake(frame.size.width, frame.size.height), false, UIScreen.mainScreen().scale)
+    public class func circularScaleAndCropImage(image: UIImage, frame: CGRect) -> UIImage {
+
+        let size = CGSize(width: frame.size.width, height: frame.size.height)
+        UIGraphicsBeginImageContextWithOptions(size, false, UIScreen.mainScreen().scale)
         let context = UIGraphicsGetCurrentContext()
-        
+
         let imageWidth = image.size.width
         let imageHeight = image.size.height
         let rectWidth = frame.size.width
@@ -38,26 +39,26 @@ extension UIImage {
 
         let scaleFactorX = rectWidth / imageWidth
         let scaleFactorY = rectHeight / imageHeight
-        
+
         let imageCenterX = rectWidth / 2
         let imageCenterY = rectHeight / 2
-        
+
         let radius = rectWidth / 2
-        
+
         CGContextBeginPath(context)
         CGContextAddArc(context, imageCenterX, imageCenterY, radius, 0, CGFloat(2 * M_PI), 0)
         CGContextClosePath(context)
         CGContextClip(context)
-        
+
         CGContextScaleCTM (context, scaleFactorX, scaleFactorY)
-        
-        let myRect = CGRectMake(0, 0, imageWidth, imageHeight)
+
+        let myRect = CGRect(x: 0, y: 0, width: imageWidth, height: imageHeight)
         image.drawInRect(myRect)
 
         let croppedImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-        
+
         return croppedImage
     }
-    
+
 }
