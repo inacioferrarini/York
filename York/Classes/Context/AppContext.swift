7f56135d96325eeb38b1e965608eb31130087295
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-04-21 00:05:06 -0300

    Class linted.

--- York/Classes/Context/AppContext.swift
+++ York/Classes/Context/AppContext.swift
@@ -24,13 +24,13 @@
 import UIKit
 
 public class AppContext: NSObject {
-    
+
     public let navigationController: UINavigationController
     public let coreDataStack: CoreDataStack
     public let syncRules: DataSyncRules
     public let router: NavigationRouter
     public let logger: Logger
-    
+
     public init(navigationController: UINavigationController, coreDataStack: CoreDataStack, syncRules: DataSyncRules, router: NavigationRouter, logger: Logger) {
         self.navigationController = navigationController
         self.coreDataStack = coreDataStack
@@ -39,5 +39,5 @@ public class AppContext: NSObject {
         self.logger = logger
         super.init()
     }
-    
+
 }
