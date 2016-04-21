af4421cf3f153f987564501c5a6a6aa0fc60dfe3
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-04-20 23:58:12 -0300

    Class linted.

--- York/Classes/DataSyncRules/Entities/EntityDailySyncRule.swift
+++ York/Classes/DataSyncRules/Entities/EntityDailySyncRule.swift
@@ -26,44 +26,44 @@ import CoreData
 
 public class EntityDailySyncRule: EntityBaseSyncRules {
 
-    public class func fetchEntityDailySyncRuleByName(name: String, inManagedObjectContext context:NSManagedObjectContext) -> EntityDailySyncRule? {
-        
+    public class func fetchEntityDailySyncRuleByName(name: String, inManagedObjectContext context: NSManagedObjectContext) -> EntityDailySyncRule? {
+
         guard name.characters.count > 0 else {
             return nil
         }
-        
-        let request:NSFetchRequest = NSFetchRequest(entityName: self.simpleClassName())
+
+        let request: NSFetchRequest = NSFetchRequest(entityName: self.simpleClassName())
         request.predicate = NSPredicate(format: "name = %@", name)
-        
+
         let matches = (try! context.executeFetchRequest(request)) as! [EntityDailySyncRule]
-        if (matches.count > 0) {
+        if matches.count > 0 {
             return matches.last
         }
-        
+
         return nil
     }
-    
-    public class func entityDailySyncRuleByName(name:String, days: NSNumber?, inManagedObjectContext context:NSManagedObjectContext) -> EntityDailySyncRule? {
-        
+
+    public class func entityDailySyncRuleByName(name: String, days: NSNumber?, inManagedObjectContext context: NSManagedObjectContext) -> EntityDailySyncRule? {
+
         guard name.characters.count > 0 else {
             return nil
         }
-        
-        var entityDailySyncRule:EntityDailySyncRule? = fetchEntityDailySyncRuleByName(name, inManagedObjectContext: context)
+
+        var entityDailySyncRule: EntityDailySyncRule? = fetchEntityDailySyncRuleByName(name, inManagedObjectContext: context)
         if entityDailySyncRule == nil {
             let newEntityDailySyncRule = NSEntityDescription.insertNewObjectForEntityForName(self.simpleClassName(), inManagedObjectContext: context) as! EntityDailySyncRule
             newEntityDailySyncRule.name = name
             entityDailySyncRule = newEntityDailySyncRule
         }
-        
+
         if let entityDailySyncRule = entityDailySyncRule {
             entityDailySyncRule.days = days
         }
-        
+
         return entityDailySyncRule
     }
-    
-    override public func shouldRunSyncRuleWithName(name:String, date:NSDate, inManagedObjectContext context:NSManagedObjectContext) -> Bool {
+
+    override public func shouldRunSyncRuleWithName(name: String, date: NSDate, inManagedObjectContext context: NSManagedObjectContext) -> Bool {
         let lastExecution = EntitySyncHistory.fetchEntityAutoSyncHistoryByName(name, inManagedObjectContext: context)
         if let lastExecution = lastExecution,
             let lastExecutionDate = lastExecution.lastExecutionDate,
