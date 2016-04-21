03003570db7d0dd9611c05a79f0eb7efd0f4df12
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-04-20 23:48:59 -0300

    Class linted.

--- York/Classes/DeepLinkingNavigation/NavigationRouter.swift
+++ York/Classes/DeepLinkingNavigation/NavigationRouter.swift
@@ -25,11 +25,11 @@ import JLRoutes
 import UIKit
 
 public class NavigationRouter: NSObject {
-    
-    public let router:JLRoutes
-    public let schema:String
-    public let logger:Logger
-    
+
+    public let router: JLRoutes
+    public let schema: String
+    public let logger: Logger
+
     public init(router: JLRoutes, schema: String, logger: Logger) {
         self.router = router
         self.schema = schema
@@ -37,36 +37,36 @@ public class NavigationRouter: NSObject {
         super.init()
     }
 
-    public convenience init(schema:String, logger: Logger) {
+    public convenience init(schema: String, logger: Logger) {
         self.init(router: JLRoutes(forScheme: schema), schema: schema, logger: logger)
     }
-    
+
     public func registerRoutes(appRoutes: [RoutingElement]) {
-        for r:RoutingElement in appRoutes {
+        for r: RoutingElement in appRoutes {
             self.router.addRoute(r.pattern, handler: r.handler)
         }
     }
-    
-    public func dispatch(url:NSURL) -> Bool {
+
+    public func dispatch(url: NSURL) -> Bool {
         let result = self.router.canRouteURL(url)
-        if (result) {
+        if result {
             self.router.routeURL(url)
         }
         return result
     }
-    
-    public func navigateInternal(targetUrl:String) {
+
+    public func navigateInternal(targetUrl: String) {
         let completeUrl = NSURL(string: "\(self.schema):/\(targetUrl)")
         if let url = completeUrl {
             UIApplication.sharedApplication().openURL(url)
         }
     }
-    
-    public func navigateExternal(targetUrl:String) {
+
+    public func navigateExternal(targetUrl: String) {
         let completeUrl = NSURL(string: targetUrl)
         if let url = completeUrl {
             UIApplication.sharedApplication().openURL(url)
         }
     }
-    
+
 }
