59d4280819560a3fb7fc238e2ee73d3a0e5db655
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-04-24 13:19:27 -0300

    Common FetcherDataSource code moved to a parent class. Adding Array extension. Adding classes used by CollectionView. Adding presenter for UICollectionView.

---
+++ York/Classes/DataSource/CoreData/CollectionViewFetcherDataSource.swift
@@ -0,0 +1,245 @@
+//    The MIT License (MIT)
+//
+//    Copyright (c) 2016 Inácio Ferrarini
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the "Software"), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in all
+//    copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//    SOFTWARE.
+//
+
+import UIKit
+import CoreData
+import York
+
+public class CollectionViewFetcherDataSource<CellType: UICollectionViewCell, EntityType: NSManagedObject>: FetcherDataSource<EntityType>, UICollectionViewDataSource {
+    
+    // MARK: - Properties
+    
+    public let collectionView: UICollectionView
+    public let presenter: CollectionViewCellPresenter<CellType, EntityType>
+    
+    // MARK: - Initialization
+    
+    public convenience init(targetingCollectionView collectionView: UICollectionView,
+                presenter: CollectionViewCellPresenter<CellType, EntityType>,
+                entityName: String,
+                sortDescriptors: [NSSortDescriptor],
+                managedObjectContext context: NSManagedObjectContext,
+                logger: Logger) {
+        
+        self.init(targetingCollectionView: collectionView,
+                  presenter: presenter,
+                  entityName: entityName,
+                  sortDescriptors: sortDescriptors,
+                  managedObjectContext: context,
+                  logger: logger)
+    }
+    
+    public init(targetingCollectionView collectionView: UICollectionView,
+                presenter: CollectionViewCellPresenter<CellType, EntityType>,
+                entityName: String,
+                sortDescriptors: [NSSortDescriptor],
+                managedObjectContext context: NSManagedObjectContext,
+                logger: Logger,
+                predicate: NSPredicate?,
+                fetchLimit: NSInteger?,
+                sectionNameKeyPath: String?,
+                cacheName: String?) {
+        
+        self.collectionView = collectionView
+        self.presenter = presenter
+        
+        super.init(entityName: entityName,
+                   sortDescriptors: sortDescriptors,
+                   managedObjectContext: context,
+                   logger: logger,
+                   predicate: predicate,
+                   fetchLimit: fetchLimit,
+                   sectionNameKeyPath: sectionNameKeyPath,
+                   cacheName: cacheName)
+    }
+    
+    
+    // MARK: - Public Methods
+    
+    public override func refreshData() throws {
+        try super.refreshData()
+        self.collectionView.reloadData()
+    }
+    
+    
+    // MARK: - Collection View Data Source
+
+    public func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return self.fetchedResultsController.sections?.count ?? 0
+    }
+    
+    public func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if let sections = self.fetchedResultsController.sections {
+            let sectionInfo = sections[section]
+            return sectionInfo.numberOfObjects
+        }
+        return 0
+    }
+    
+    public func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let value = self.objectAtIndexPath(indexPath)
+
+        let reuseIdentifier = self.presenter.cellReuseIdentifierBlock(indexPath: indexPath)
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as? CellType
+        if let cell = cell,
+            let value = value {
+            self.presenter.configureCellBlock(cell, value)
+        }
+        return cell!
+    }
+
+
+    // MARK: - private properties
+    
+    private var deletedSectionIndexes = NSMutableIndexSet()
+    private var insertedSectionIndexes = NSMutableIndexSet()
+    private var deletedItemIndexPaths = [NSIndexPath]()
+    private var insertedItemIndexPaths = [NSIndexPath]()
+    private var updatedItemIndexPaths = [NSIndexPath]()
+    
+    
+    // MARK: - Fetched results controller
+    
+    public func controller(controller: NSFetchedResultsController, didChangeSection sectionInfo: NSFetchedResultsSectionInfo,
+                           atIndex sectionIndex: Int, forChangeType type: NSFetchedResultsChangeType) {
+        
+        switch type {
+        case .Insert:
+            self.insertedSectionIndexes.addIndex(sectionIndex)
+            break
+        case .Delete:
+            self.deletedSectionIndexes.addIndex(sectionIndex)
+            var indexPathsInSection = [NSIndexPath]()
+            
+            for indexPath in self.deletedItemIndexPaths {
+                if indexPath.section == sectionIndex {
+                    indexPathsInSection.append(indexPath)
+                }
+            }
+            self.deletedItemIndexPaths.removeObjectsInArray(indexPathsInSection)
+            indexPathsInSection.removeAll()
+            
+            for indexPath in self.updatedItemIndexPaths {
+                if indexPath.section == sectionIndex {
+                    indexPathsInSection.append(indexPath)
+                }
+            }
+            self.updatedItemIndexPaths.removeObjectsInArray(indexPathsInSection)
+
+            break
+        default:
+            break
+        }
+    }
+
+    public func controller(controller: NSFetchedResultsController, didChangeObject anObject: AnyObject,
+                           atIndexPath indexPath: NSIndexPath?,
+                           forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
+        
+        switch type {
+        case .Insert:
+            if let newIndexPath = newIndexPath {
+                if self.insertedSectionIndexes.containsIndex(newIndexPath.section) {
+                    return
+                }
+                self.insertedItemIndexPaths.append(newIndexPath)
+            }
+            break
+        case .Delete:
+            if let indexPath = indexPath {
+                if self.deletedSectionIndexes.containsIndex(indexPath.section) {
+                    return
+                }
+                self.deletedItemIndexPaths.append(indexPath)
+            }
+            break
+        case .Move:
+            if let indexPath = indexPath,
+                let newIndexPath = newIndexPath {
+                if !self.insertedSectionIndexes.containsIndex(newIndexPath.section) {
+                    self.insertedItemIndexPaths.append(newIndexPath)
+                }
+                if !self.deletedSectionIndexes.containsIndex(indexPath.section) {
+                    self.deletedItemIndexPaths.append(indexPath)
+                }
+            }
+            break;
+        case .Update:
+            if let indexPath = indexPath {
+                if self.deletedSectionIndexes.containsIndex(indexPath.section) || self.deletedItemIndexPaths.contains(indexPath) {
+                    return
+                }
+                if !self.updatedItemIndexPaths.contains(indexPath) {
+                    self.updatedItemIndexPaths.append(indexPath)
+                }
+            }
+            break
+        }
+    }
+
+    public func controllerDidChangeContent(controller: NSFetchedResultsController) {
+        self.commitChanges()
+    }
+    
+    
+    // MARK: - Collection View Helper
+    
+    public func commitChanges() {
+
+        if self.collectionView.window == nil {
+            self.clearChanges()
+            self.collectionView.reloadData()
+        }
+        
+        let totalChanges = (self.deletedSectionIndexes.count ?? 0) +
+            (self.insertedSectionIndexes.count ?? 0) +
+            (self.deletedItemIndexPaths.count ?? 0) +
+            (self.insertedItemIndexPaths.count ?? 0) +
+            (self.updatedItemIndexPaths.count ?? 0)
+        
+        if totalChanges > 50 {
+            self.clearChanges()
+            self.collectionView.reloadData()
+            return
+        }
+        
+        self.collectionView.performBatchUpdates({
+            self.collectionView.deleteSections(self.deletedSectionIndexes)
+            self.collectionView.insertSections(self.insertedSectionIndexes)
+            self.collectionView.deleteItemsAtIndexPaths(self.deletedItemIndexPaths)
+            self.collectionView.insertItemsAtIndexPaths(self.insertedItemIndexPaths)
+            self.collectionView.reloadItemsAtIndexPaths(self.updatedItemIndexPaths)
+        }) { (finished: Bool) in
+            self.clearChanges()
+        }
+    }
+
+    public func clearChanges() {
+        self.insertedSectionIndexes.removeAllIndexes()
+        self.deletedSectionIndexes.removeAllIndexes()
+        self.deletedItemIndexPaths.removeAll()
+        self.insertedItemIndexPaths.removeAll()
+        self.updatedItemIndexPaths.removeAll()
+    }
+    
+}
---
+++ York/Classes/DataSource/CoreData/FetcherDataSource.swift
@@ -0,0 +1,135 @@
+//    The MIT License (MIT)
+//
+//    Copyright (c) 2016 Inácio Ferrarini
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the "Software"), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in all
+//    copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//    SOFTWARE.
+//
+
+import UIKit
+import CoreData
+import York
+
+public class FetcherDataSource<EntityType: NSManagedObject>: NSObject, NSFetchedResultsControllerDelegate {
+    
+    // MARK: - Properties
+
+    public private(set) var entityName: String
+    public var predicate: NSPredicate? {
+        didSet {
+            self.fetchedResultsController.fetchRequest.predicate = predicate
+        }
+    }
+    public var fetchLimit: NSInteger?
+    public var sortDescriptors: [NSSortDescriptor] {
+        didSet {
+            self.fetchedResultsController.fetchRequest.sortDescriptors = sortDescriptors
+        }
+    }
+    public var sectionNameKeyPath: String?
+    public var cacheName: String?
+    public let managedObjectContext: NSManagedObjectContext
+    public let logger: Logger
+
+
+    // MARK: - Initialization
+    
+    public convenience init(entityName: String,
+                sortDescriptors: [NSSortDescriptor],
+                managedObjectContext context: NSManagedObjectContext,
+                logger: Logger) {
+        
+        self.init(entityName: entityName,
+                  sortDescriptors: sortDescriptors,
+                  managedObjectContext: context,
+                  logger:logger,
+                  predicate: nil,
+                  fetchLimit: nil,
+                  sectionNameKeyPath: nil,
+                  cacheName: nil)
+    }
+    
+    public init(entityName: String,
+                sortDescriptors: [NSSortDescriptor],
+                managedObjectContext context: NSManagedObjectContext,
+                logger: Logger,
+                predicate: NSPredicate?,
+                fetchLimit: NSInteger?,
+                sectionNameKeyPath: String?,
+                cacheName: String?) {
+        
+        self.entityName = entityName
+        self.sortDescriptors = sortDescriptors
+        self.managedObjectContext = context
+        self.logger = logger
+        self.predicate = predicate
+        self.fetchLimit = fetchLimit
+        self.sectionNameKeyPath = sectionNameKeyPath
+        self.cacheName = cacheName
+        super.init()
+    }
+    
+    
+    // MARK: - Public Methods
+    
+    public func refreshData() throws {
+        try self.fetchedResultsController.performFetch()
+    }
+    
+    public func objectAtIndexPath(indexPath: NSIndexPath) -> EntityType? {
+        return self.fetchedResultsController.objectAtIndexPath(indexPath) as? EntityType
+    }
+    
+    public func indexPathForObject(object: EntityType) -> NSIndexPath? {
+        return self.fetchedResultsController.indexPathForObject(object)
+    }
+    
+    
+    // MARK: - Fetched results controller
+    
+    public var fetchedResultsController: NSFetchedResultsController {
+        
+        if _fetchedResultsController != nil {
+            return _fetchedResultsController!
+        }
+        
+        let fetchRequest = NSFetchRequest()
+        let entity = NSEntityDescription.entityForName(self.entityName, inManagedObjectContext: self.managedObjectContext)
+        fetchRequest.entity = entity
+        
+        fetchRequest.predicate = self.predicate
+        
+        if let fetchLimit = self.fetchLimit where fetchLimit > 0 {
+            fetchRequest.fetchLimit = fetchLimit
+        }
+        
+        fetchRequest.fetchBatchSize = 100
+        fetchRequest.sortDescriptors = self.sortDescriptors
+        
+        let aFetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest,
+                                                                   managedObjectContext: self.managedObjectContext,
+                                                                   sectionNameKeyPath: self.sectionNameKeyPath,
+                                                                   cacheName: self.cacheName)
+        aFetchedResultsController.delegate = self
+        _fetchedResultsController = aFetchedResultsController
+        
+        return _fetchedResultsController!
+    }
+    private var _fetchedResultsController: NSFetchedResultsController? = nil
+    
+}
