d6df501bf581c7aedb958fe426602ee0a7f87225
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-04-24 18:25:47 -0300

    Adding tests for CollectionViewCellPresenter.

---
+++ Example/Tests/York/Presenter/CollectionViewCellPresenterTests.swift
@@ -0,0 +1,68 @@
+//    The MIT License (MIT)
+//
+//    Copyright (c) 2016 Inácio Ferrarini
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the "Software"), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in all
+//    copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//    SOFTWARE.
+//
+
+import XCTest
+import CoreData
+import York
+
+class CollectionViewCellPresenterTests: XCTestCase {
+    
+    static let modelFileName = "DataSyncRules"
+    static let databaseFileName = "DataSyncHourlyRulesTests"
+    
+    static let cellReuseIdBlock: ((indexPath: NSIndexPath) -> String) = { (indexPath: NSIndexPath) -> String in
+        return "ReuseCellID"
+    }
+    
+    static var blockExecutionTest = ""
+    
+    func createCollectionViewCellPresenter() -> CollectionViewCellPresenter<UICollectionViewCell, EntitySyncHistory> {
+        
+        let presenter = CollectionViewCellPresenter<UICollectionViewCell, EntitySyncHistory>(
+            configureCellBlock: { (cell: UICollectionViewCell, entity:EntitySyncHistory) -> Void in
+                CollectionViewCellPresenterTests.blockExecutionTest = "testExecuted"
+            }, cellReuseIdentifierBlock: CollectionViewCellPresenterTests.cellReuseIdBlock)
+        return presenter
+    }
+    
+    func test_collectionViewCellPresenterFields_configureCellBlock() {
+        let yorkBundle = TestUtil().yorkPODBundle()
+        let logger = Logger()
+        let dataSyncRulesStack = CoreDataStack(modelFileName: CollectionViewCellPresenterTests.modelFileName, databaseFileName: CollectionViewCellPresenterTests.databaseFileName, logger: logger,
+                                               bundle: yorkBundle)
+        let presenter = self.createCollectionViewCellPresenter()
+        let cell = UICollectionViewCell()
+        let ctx = dataSyncRulesStack.managedObjectContext
+        let obj = EntitySyncHistory.entityAutoSyncHistoryByName("xpto", lastExecutionDate: nil, inManagedObjectContext: ctx)!
+        presenter.configureCellBlock(cell, obj)
+        XCTAssertEqual(CollectionViewCellPresenterTests.blockExecutionTest, "testExecuted")
+    }
+    
+    func test_collectionViewCellPresenterFields_reuseIdentifier() {
+        let presenter = self.createCollectionViewCellPresenter()
+        let presenterReuseId = presenter.cellReuseIdentifierBlock(indexPath: NSIndexPath(forRow: 0, inSection: 0))
+        let testReuseId = CollectionViewCellPresenterTests.cellReuseIdBlock(indexPath: NSIndexPath(forRow: 0, inSection: 0))
+        XCTAssertEqual(presenterReuseId, testReuseId)
+    }
+    
+}
