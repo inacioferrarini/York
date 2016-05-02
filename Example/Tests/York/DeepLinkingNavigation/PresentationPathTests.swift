a02c58bbddcb495c2b1a74b0adc48033121c433e
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-05-01 21:43:38 -0300

    Including PresentationPath to abstract the way the urls are used and abstract presentation modes passed as parameters.

---
+++ Example/Tests/York/DeepLinkingNavigation/PresentationPathTests.swift
@@ -0,0 +1,117 @@
+//    The MIT License (MIT)
+//
+//    Copyright (c) 2016 Inácio Ferrarini
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the "Software"), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in all
+//    copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//    SOFTWARE.
+//
+
+import XCTest
+import York
+
+class PresentationPathTests: XCTestCase {
+
+
+    // MARK: - Tests
+
+    func test_absoluteString_mustReturnPresentationParameter() {
+        let presentationPath = PresentationPath(path: "/path/to/viewcontroller")
+        let stringPath = presentationPath.absoluteString()
+        XCTAssertEqual(stringPath, "/path/to/viewcontroller/:presentation")
+    }
+
+    func test_absoluteStringEndingSlash_mustReturnPresentationParameter() {
+        let presentationPath = PresentationPath(path: "/path/to/viewcontroller/")
+        let stringPath = presentationPath.absoluteString()
+        XCTAssertEqual(stringPath, "/path/to/viewcontroller/:presentation")
+    }
+    
+    
+    // MARK: - Tests - PresentationMode
+    
+    func test_presentationModeForPath_withoutPresentationMode_mustReturnPush() {
+        let presentationPath = PresentationPath(path: "/path/to/viewcontroller/")
+        let presentationMode = presentationPath.presentationMode()
+        XCTAssertEqual(presentationMode, PresentationPathMode.Push)
+    }
+    
+    func test_presentationModeForPath_withPushPresentationMode_mustReturnPush() {
+        let presentationPath = PresentationPath(path: "/path/to/viewcontroller/push")
+        let presentationMode = presentationPath.presentationMode()
+        XCTAssertEqual(presentationMode, PresentationPathMode.Push)
+    }
+    
+    func test_presentationModeForPath_withShowPresentationMode_mustReturnShow() {
+        let presentationPath = PresentationPath(path: "/path/to/viewcontroller/show")
+        let presentationMode = presentationPath.presentationMode()
+        XCTAssertEqual(presentationMode, PresentationPathMode.Show)
+    }
+
+    func test_presentationModeForPath_withModalPresentationMode_mustReturnModal() {
+        let presentationPath = PresentationPath(path: "/path/to/viewcontroller/modal")
+        let presentationMode = presentationPath.presentationMode()
+        XCTAssertEqual(presentationMode, PresentationPathMode.Modal)
+    }
+
+    func test_presentationModeForPath_withPopoverPresentationMode_mustReturnPopover() {
+        let presentationPath = PresentationPath(path: "/path/to/viewcontroller/popover")
+        let presentationMode = presentationPath.presentationMode()
+        XCTAssertEqual(presentationMode, PresentationPathMode.Popover)
+    }
+    
+    
+    // MARK: - Tests - PresentationMode
+    
+    func test_pathWithValuesAndPresentationMode_mustSucceed() {
+        let presentationPath = PresentationPath(path: "/:param1/:param2/:param3/")
+        let values = [ ":param1": "valueForParam1", ":param2": "valueForParam2", ":param3": "valueForParam3" ]
+        let finalUrl = presentationPath.replacingValues(values, mode: .Push)        
+        XCTAssertEqual(finalUrl, "/valueForParam1/valueForParam2/valueForParam3/push")
+    }
+
+    
+    
+    
+//    public func path(mode: PresentationPathMode, values: [String : String]) -> String {
+//        var path = self.removeLastPathSeparator(self.path)
+//        
+//        for (key, value) in values {
+//            path = path.stringByReplacingOccurrencesOfString(key, withString: value, options: .CaseInsensitiveSearch, range: nil)
+//        }
+//        
+//        return self.removeLastPathSeparator(self.path) + "/" + mode.rawValue
+//    }
+
+    
+    
+    
+    
+//    public func presentationModeForPath(path: PresentationPath) -> PresentationPathMode {
+//        var mode:PresentationPathMode = .Push
+//        let stringPath = path.absoluteString()
+//        if let lastParameterPathIndexRange = stringPath.rangeOfString(":", options: .BackwardsSearch) {
+//            let lastParameterPathIndex = lastParameterPathIndexRange.startIndex
+//            let stringValue = stringPath.substringFromIndex(lastParameterPathIndex)
+//            if let value = PresentationPathMode(rawValue: stringValue) {
+//                mode = value
+//            }
+//        }
+//        return mode
+//    }
+
+}
