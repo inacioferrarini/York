6cedd1ce280d51046106a07d51f7f768df96244d
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-05-01 22:46:55 -0300

    Updating path from string to PresentationPath, since it allows an better degree of customization.

--- York/Classes/DeepLinkingNavigation/RoutingElement.swift
+++ York/Classes/DeepLinkingNavigation/RoutingElement.swift
@@ -25,11 +25,11 @@ import UIKit
 
 public class RoutingElement: NSObject {
 
-    public let pattern: String
+    public let path: PresentationPath
     public let handler: (([NSObject : AnyObject]) -> Bool)
 
-    public init(pattern: String, handler: (([NSObject : AnyObject]) -> Bool)) {
-        self.pattern = pattern
+    public init(path: PresentationPath, handler: (([NSObject : AnyObject]) -> Bool)) {
+        self.path = path
         self.handler = handler
         super.init()
     }
