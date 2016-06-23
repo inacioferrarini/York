edb88d1804c2233466b04de7f1b0730fc4f5abe3
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-06-23 18:35:48 -0300

    Adding ArrayOfArrayDataSource and TableViewArrayOfArrayDataSource.

---
+++ Classes/DataSource/ArraysOfArrays/TableViewArrayOfArrayDataSource.swift
@@ -0,0 +1,99 @@
+//    The MIT License (MIT)
+//
+//    Copyright (c) 2016 Inácio Ferrarini
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the "Software"), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in all
+//    copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//    SOFTWARE.
+//
+
+import UIKit
+
+public class TableViewArrayOfArrayDataSource<CellType: UITableViewCell, Type: Equatable>: ArrayOfArrayDataSource<Type>, UITableViewDataSource {
+
+
+    // MARK: - Properties
+
+    public let tableView: UITableView
+    public private(set) var presenter: TableViewCellPresenter<CellType, Type>
+    public var titleForHeaderInSectionBlock: ((section: Int) -> String?)?
+
+
+    // MARK: - Initialization
+
+    public init(targetingTableView tableView: UITableView,
+                                   presenter: TableViewCellPresenter<CellType, Type>,
+                                   objects: [[Type]]) {
+
+        self.tableView = tableView
+        self.presenter = presenter
+
+        super.init(objects: objects)
+    }
+
+
+    // MARK: - Public Methods
+
+    public override func refreshData() {
+        super.refreshData()
+        self.tableView.reloadData()
+    }
+
+
+    // MARK: - Table View Data Source
+
+public func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return self.objects.count
+}
+
+    public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard section < self.objects.count else { return 0 }
+        return self.objects[section].count
+    }
+
+    public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let value = self.objectAtIndexPath(indexPath)
+        let reuseIdentifier = self.presenter.cellReuseIdentifierBlock(indexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier(reuseIdentifier, forIndexPath: indexPath) as? CellType
+        if let cell = cell,
+            let value = value {
+            self.presenter.configureCellBlock(cell, value)
+        }
+        return cell!
+    }
+
+    public func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if let titleBlock = self.titleForHeaderInSectionBlock {
+            return titleBlock(section: section)
+        }
+        return nil
+    }
+
+    public func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        if let editRowBlock = self.presenter.canEditRowAtIndexPathBlock {
+            return editRowBlock(indexPath: indexPath)
+        }
+        return false
+    }
+
+    public func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if let commitEditingBlock = self.presenter.commitEditingStyleBlock {
+            commitEditingBlock(editingStyle: editingStyle, forRowAtIndexPath: indexPath)
+        }
+    }
+
+}
---
+++ Example/Tests/York/DataSource/ArraysOfArrays/ArrayOfArrayDataSourceTests.swift
@@ -0,0 +1,89 @@
+//    The MIT License (MIT)
+//
+//    Copyright (c) 2016 Inácio Ferrarini
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the "Software"), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in all
+//    copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//    SOFTWARE.
+//
+
+import XCTest
+import York
+
+class ArrayOfArrayDataSourceTests: XCTestCase {
+
+
+    // MARK: - Supporting Methods
+
+    func createArrayOfArrayDataSource() -> ArrayOfArrayDataSource<NSNumber> {
+        return ArrayOfArrayDataSource<NSNumber>(objects: [[10], [10, 20], [10, 20, 30, 40]])
+    }
+
+
+    // MARK: - Tests - Initialization
+
+    func test_initializer_withObjects() {
+        let dataSource = self.createArrayOfArrayDataSource()
+        XCTAssertEqual(dataSource.objects, [[10], [10, 20], [10, 20, 30, 40]])
+    }
+
+
+    // MARK: - Tests - refresh
+
+    func test_refresh_mustSucceed() {
+        let dataSource = self.createArrayOfArrayDataSource()
+        dataSource.refreshData()
+    }
+
+
+    // MARK: - Tests - objectAtIndexPath
+
+    func test_objectAtIndexPath_mustReturnObject() {
+        let dataSource = self.createArrayOfArrayDataSource()
+        let value = dataSource.objectAtIndexPath(NSIndexPath(forRow: 2, inSection: 2))
+        XCTAssertEqual(value, 30)
+    }
+
+    func test_objectAtIndexPath_sectionGreaterThanZero_mustReturnNil() {
+        let dataSource = self.createArrayOfArrayDataSource()
+        let value = dataSource.objectAtIndexPath(NSIndexPath(forRow: 2, inSection: 3))
+        XCTAssertNil(value)
+    }
+
+    func test_objectAtIndexPath_nonExistingSection_mustReturnNil() {
+        let dataSource = self.createArrayOfArrayDataSource()
+        let value = dataSource.objectAtIndexPath(NSIndexPath(forRow: 10, inSection: 5))
+        XCTAssertNil(value)
+    }
+
+    func test_objectAtIndexPath_nonExistingIndex_mustReturnNil() {
+        let dataSource = self.createArrayOfArrayDataSource()
+        let value = dataSource.objectAtIndexPath(NSIndexPath(forRow: 10, inSection: 0))
+        XCTAssertNil(value)
+    }
+
+
+    // MARK: - Tests - indexPathForObject
+
+    func test_indexPathForObject_mustSucceed() {
+        let dataSource = self.createArrayOfArrayDataSource()
+        let indexPath = dataSource.indexPathForObject(30)
+        let desiredIndexPath = NSIndexPath(forRow: 2, inSection: 2)
+        XCTAssertEqual(desiredIndexPath, indexPath)
+    }
+
+}
