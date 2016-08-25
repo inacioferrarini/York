994968abf1f49faa20d7f79749461263655bb42b
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2016-08-25 15:20:53 -0300

    Trying to improve the static data structure.

--- Classes/StaticContent/TableView/StaticTableViewStructure.swift
+++ Classes/StaticContent/TableView/StaticTableViewStructure.swift
@@ -28,6 +28,7 @@ public struct StaticTableViewStructure {
     public var structureUid: String
     public var height: CGFloat?
     public var cellReuseIdentifier: String
+    public var data: [String : Any]?
     public var configureCellBlock: ((cell: UITableViewCell, structure: StaticTableViewStructure) -> Void)?
 
     public init(structureUid: String, cellReuseIdentifier: String) {
